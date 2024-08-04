45e5557eafa8feddf02653415d37089df77ba17a
Evgeniy <ev.shipitsyn@yandex.ru>
2024-08-04 17:24:58 +0300

    work with result logic

--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -8,13 +8,40 @@
 import UIKit
 
 final class ResultViewController: UIViewController {
+    
+    // MARK: - IB Outlets
+    @IBOutlet var animalLabel: UILabel!
+    @IBOutlet var definitionLabel: UILabel!
+    
+    // MARK: - Public Properties
+    var result: [Answer]!
 
+    // MARK: - View Life Cycles
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        navigationItem.hidesBackButton = true
+        getAnimal()
     }
     
+    // MARK: - IB Actions
     @IBAction func doneButtonAction(_ sender: UIBarButtonItem) {
         dismiss(animated: true)
+    }
+}
+
+// MARK: - Private Methods
+private extension ResultViewController {
+     func getAnimal() {
+         let animals = result.map { $0.animal }
+         let animalCounts = Dictionary(animals.map { ($0, 1) }, uniquingKeysWith: +)
+
+         if let topAnimal = animalCounts.max(by: { $0.value < $1.value })?.key {
+             updateUI(with: topAnimal)
+         }
+    }
+    
+     func updateUI(with topAnimal: Animal) {
+        animalLabel.text = "Вы - \(topAnimal.rawValue)!"
+        definitionLabel.text = topAnimal.definition
     }
 }
